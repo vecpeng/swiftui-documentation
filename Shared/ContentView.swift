42f109aa28eb6785db6195fcb99dc4ee9a96cad2
vecpeng <841298391@qq.com>
2021-12-16 21:43:27 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by peng vec on 2021/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swiftui_documentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_documentationApp.swift
+//  Shared
+//
+//  Created by peng vec on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_documentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
